bc38c7070bbecc293708caf3e04546385bab40c6
Peter Kim <kroits09@gmail.com>
2020-12-16 15:08:17 +0900

    Add updateCache for login and register mutation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,42 @@
 import React from "react";
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Flex, Link, Button } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
-  // const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
 
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        {" "}
         <NextLink href="/login">
           <Link mr={2}>login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link>register</Link>
-        </NextLink>
-      </Box>
+        </NextLink>{" "}
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
