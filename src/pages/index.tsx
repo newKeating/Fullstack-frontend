5e053d128aa4806ecc5b7ee2cd5112a5bfa8fe6d
Peter Kim <kroits09@gmail.com>
2020-12-21 21:48:51 +0900

    Switch text field of posts query to textSnippet fieldResolver

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,36 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
-import { Button } from "@chakra-ui/react";
+import { Button, Stack, Box, Heading, Text, Flex } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Button colorScheme="teal" variant="solid">
-          Create Post
-        </Button>
-      </NextLink>
-      <br />
+      <Flex justify="flex-end">
+        <NextLink href="/create-post">
+          <Button colorScheme="teal" variant="solid">
+            Create Post
+          </Button>
+        </NextLink>
+      </Flex>
+
       {!data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+        <Stack spacing={8} mt={4}>
+          {data.posts.map((post) => (
+            <Box key={post.id} p={4} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   );
